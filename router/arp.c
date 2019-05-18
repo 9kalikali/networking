d5bcccbacba0429914eb323b4683dba2ea89107c
Xuanzhengbo Ren <nanasumrai@gmail.com>
2019-05-18 17:21:53 +0800

    arp请求和应答已完成

--- router/arp.c
+++ router/arp.c
@@ -4,7 +4,9 @@
 #include "packet.h"
 #include "ether.h"
 #include "arpcache.h"
-
+#include "icmp.h"
+#include "ip.h"
+#include "rtable.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,19 +17,86 @@
 // iface_send_packet
 void arp_send_request(iface_info_t *iface, u32 dst_ip)
 {
-	fprintf(stderr, "TODO: send arp request when lookup failed in arpcache.\n");
+	//fprintf(stderr, "TODO: send arp request when lookup failed in arpcache.\n");
+	char* packet;
+	struct ether_header* eh;
+
+	packet = (char*)malloc(ETHER_HDR_SIZE + sizeof(ether_arp));
+	eh = (struct ether_header*)(packet);
+	memccpy(eh->ether_shost, iface->mac, ETH_ALEN);
+	u8 tmp = 255;
+	for(int q = 0; q < ETH_ALEN; ++q)
+		eh-> ether_dhost[q] = tmp;
+	eh->ether_type = htons(ETH_P_ARP);
+	// set-up arp protocol
+	struct ether_arp* eh_arp;
+	eh_arp = (strcut ether_arp*)(packet + ETHER_HDR_SIZE);
+	eh_arp->arp_hrd = htons((u16)0x01);
+	eh_arp->arp)_pro = htons((u16)0x0800);
+	eh_arp->arp_hln = (u8)6;
+	eh_arp->arp_pln = (u8)4;
+	eh_arp->arp_op = htons((u16)0x01);
+	memcpy(eh_arp->arp_sha, iface->mac, ETH_ALEN);
+	eh_arp->arp_spa = htol(iface->ip);
+	eh_arp->arp_tpa = htol(dst_ip);
+	memset(eh_arp->arp_tha, 0, ETH_ALEN);
+	iface_send_packet(iface, packet, (ETHER_HDR_SIZE + sizeof(struct ether_arp)));
 }
 
 // send an arp reply packet: encapsulate an arp reply packet, send it out
 // through iface_send_packet
 void arp_send_reply(iface_info_t *iface, struct ether_arp *req_hdr)
 {
-	fprintf(stderr, "TODO: send arp reply when receiving arp request.\n");
+	//fprintf(stderr, "TODO: send arp reply when receiving arp request.\n");
+	char* packet;
+	struct ether_header* eh;
+
+	packet = (char*)malloc(ETHER_HDR_SIZE + sizeof(struct ether_arp));
+	eh = (struct ether_header*)(packet);
+	eh->ether_type = htons(ETH_P_ARP);
+
+	struct ether_arp* eh_arp;
+	u32 my_ip = ntohl(req_hdr->arp_spa);
+
+	eh_arp = (struct eh_arp*)(packet + ETHER_HDR_SIZE);
+	memcpy(eh->ether_shost, iface->mac, ETH_ALEN);
+	memcpy(eh->ether_dhost, req_hdr->arp_sha, ETH_ALEN);
+	eh_arp->arp_hrd = htons((u16)0x01);
+	eh_arp->arp)_pro = htons((u16)0x0800);
+	eh_arp->arp_hln = (u8)6;
+	eh_arp->arp_pln = (u8)4;
+	eh_arp->arp_op = htons(ARPOP_REPLY);
+	eh_arp->arp_spa = htonl(iface->ip);
+	eh_arp->arp_tpa = htonl(my_ip);
+	memcpy(eh_arp->arp_sha, iface->mac, ETH_ALEN);
+	memccpy(eh_arp->arp_tha, req_hdr->arp_sha, ETH_ALEN);
+
+	iface_send_packet(iface, packet, (ETHER_HDR_SIZE + sizeof(struct ether_arp)));
 }
 
 void handle_arp_packet(iface_info_t *iface, char *packet, int len)
 {
-	fprintf(stderr, "TODO: process arp packet: arp request & arp reply.\n");
+	//fprintf(stderr, "TODO: process arp packet: arp request & arp reply.\n");
+	struct ether_header* eh;
+	struct ether_arp* eh_arp;
+
+	eh = (struct ether_header*)(packet);
+	eh_arp = (struct ether_arp*)(packet + ETHER_HDR_SIZE);
+	if(ntohs(eh_arp->arp_op) == 0x01)
+	{
+		if(ntohl(eh_arp->arp_tpa) == iface->ip)
+		{
+			arp_send_reply(iface, eh_arp);
+			arpcache_insert(ntohl(eh_arp->arp_spa), eh_arp->arp_sha);
+		}
+	}
+	else if(ntohs(eh_arp->arp_op) == 0x02)
+	{
+		if(ntohl(eh_arp->arp_tpa) == iface->ip)
+			arpcache_insert(ntohl(eh_arp->arp_spa), eh_arp->arp_sha);
+	}
+
+	free(packet);
 }
 
 // send (IP) packet through arpcache lookup 
