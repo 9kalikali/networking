18a51eec354055dd00b71949ec9e27b80bea3b65
Xuanzhengbo Ren <nanasumrai@gmail.com>
2019-05-27 22:00:10 +0800

    turn off debug log

--- router/main.c
+++ router/main.c
@@ -41,8 +41,8 @@ void handle_packet(iface_info_t *iface, char *packet, int len)
 {
 	struct ether_header *eh = (struct ether_header *)packet;
 
-	log(DEBUG, "got packet from %s, %d bytes, proto: 0x%04hx\n", 
-	 		iface->name, len, ntohs(eh->ether_type));
+	// log(DEBUG, "got packet from %s, %d bytes, proto: 0x%04hx\n", 
+	// 		iface->name, len, ntohs(eh->ether_type));
 	switch (ntohs(eh->ether_type)) {
 		case ETH_P_IP:
 			handle_ip_packet(iface, packet, len);
@@ -51,8 +51,8 @@ void handle_packet(iface_info_t *iface, char *packet, int len)
 			handle_arp_packet(iface, packet, len);
 			break;
 		default:
-			log(ERROR, "Unknown packet type 0x%04hx, ingore it.", \
-					ntohs(eh->ether_type));
+			//log(ERROR, "Unknown packet type 0x%04hx, ingore it.", \
+			//		ntohs(eh->ether_type));
 			break;
 	}
 }
@@ -178,7 +178,7 @@ static void find_available_ifaces()
 	list_for_each_entry(iface, &instance->iface_list, list) {
 		sprintf(dev_names + strlen(dev_names), " %s", iface->name);
 	}
-	log(DEBUG, "find the following interfaces: %s.", dev_names);
+	// log(DEBUG, "find the following interfaces: %s.", dev_names);
 }
 
 // read the information of all interfaces, and store them in iface_list
