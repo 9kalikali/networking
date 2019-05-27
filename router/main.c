9310dab0935a9634972da71bf21ba9f9d6dae7b7
Xuanzhengbo Ren <nanasumrai@gmail.com>
2019-05-27 22:03:36 +0800

    fix warnning

--- router/main.c
+++ router/main.c
@@ -51,8 +51,7 @@ void handle_packet(iface_info_t *iface, char *packet, int len)
 			handle_arp_packet(iface, packet, len);
 			break;
 		default:
-			// log(ERROR, "Unknown packet type 0x%04hx, ingore it.", \
-			// 		ntohs(eh->ether_type));
+			
 			break;
 	}
 }
